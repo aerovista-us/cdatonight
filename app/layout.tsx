b56364c8c072c4265e2e2003d12173d46565bc68
AeroVista <aerovistaus@gmail.com>
2026-08-22 18:22:28 -0700

    Hero: load full-image fit override

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import "./globals.css";
 import "./nightlife.css";
 import "./late-eats.css";
 import "./wide-layout.css";
+import "./hero-real.css";
 
 const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || "https://cdatonight.aerovista.us";
 
